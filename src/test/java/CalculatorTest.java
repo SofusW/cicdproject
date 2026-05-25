b628bd0f9a9805acbc2fb7f422019b7994608fe3
sofusw <sfswnthr@gmail.com>
2026-05-25 21:04:28 +0200

    test

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+
+    @Test
+    void testAdd() {
+
+        Calculator calc = new Calculator();
+        assertEquals(4, calc.add(2, 2));
+
+    }
+
+}
